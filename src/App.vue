25c3971c72d88cc58b4b8139d61578bd67fd53b7
Goran Lovincic <66091550+Goran1310@users.noreply.github.com>
2023-01-16 00:04:39 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,11 @@
           @keypress="fetchWeather"
           @keyup.enter="fetchWeather"
         />
-
+        
     </div>
+    Current weather data.
+    Access current weather data for any location on Earth including over 200,000 cities.
+    Weather data from different sources such as global and local weather models, satellites, radars and a vast network of weather stations. Data is available with API call in JSON
     <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
       <div class="location-box">
         <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
@@ -22,6 +25,8 @@
       <div class="weather-box">
         <div class="temp">{{Math.round(weather.main.temp)}} °C</div>
         <div class="weather">{{ weather.weather[0].main }}</div>
+        <div class="humidity">Humidity: {{weather.main.humidity}} %</div>
+        <div class="wind">Wind speed: {{weather.wind.speed}} meter/sec</div>
       </div>
     </div>
   </main>
@@ -44,6 +49,7 @@ export default {
   methods: {
     fetchWeather(e) {
       if(e.key=='Enter') {
+        // To make an API call 
         fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
         .then(res=> {
           return res.json();
@@ -121,6 +127,7 @@ main {
   border-radius:  16px 0px 16px 0px;
 }
 .location-box .location {
+  margin-top: 2vh;
   color: #FFFF;
   font-size: 29px;
   font-weight: bold;
@@ -136,7 +143,7 @@ main {
     color: #FFFF;
   font-size: 21px;
   font-weight: bold;
-  text-shadow: rgba(0,0,0,0.3);
+  text-shadow: 3px 6px rgba(0,0,0,0.3);
 }
 .weather-box{
   color: #FFFF;
@@ -158,5 +165,10 @@ main {
   font-weight: bold;
   text-shadow: 3px 6px rgba(0,0,0,0.3);
 }
-
+.humidity, .wind {
+    color: #FFFF;
+  font-size: 21px;
+  font-weight: bold;
+  text-shadow: 2px 4px rgba(0,0,0,0.3);
+}
 </style>
